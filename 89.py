4f3edbfd572cf15f457e777eaf8d295560c3b4d5
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-12-06 11:21:18 +0530

    Update 89.py

--- 89.py
+++ 89.py
@@ -1,11 +1,11 @@
 def sort(values):
-	for  m range(len(values)):		
-		for n in range(m, len(values)):			
-			if (values[m] > values[n]):
-				temp = values[m]
-				values[m] = values[n]
-				values[n] = temp
-y = raw_input().rstrip()
-yList = list(y)
-sort(yList)
-print("".join(yList))
+	for  i range(len(values)):		
+		for r in range(i, len(values)):			
+			if (values[i] > values[r]):
+				temp = values[i]
+				values[i] = values[r]
+				values[r] = temp
+x = raw_input().rstrip()
+xList = list(x)
+sort(xList)
+print("".join(xList))
